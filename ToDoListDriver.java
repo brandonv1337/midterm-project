034d16f3e2a4b737119a7bc266c205f0d7559007
brandonv1337 <158694624+brandonv1337@users.noreply.github.com>
2025-03-12 20:39:06 -0500

    Update ToDoListDriver.java

--- ToDoListDriver.java
+++ ToDoListDriver.java
@@ -4,11 +4,8 @@ public class ToDoListDriver
 {
     public static void main(String[] args) 
     {
-
-        // DueDate date = new DueDate(1, 22, 2022);
-        // DueDate date2 = new DueDate(6, 17, 2026);
-        // DueDate date3 = new DueDate(4, 9, 2025);
-        // DueDate date4 = new DueDate(8, 23, 2026);
+        Scanner keyboard = new Scanner(System.in);
+ 
         // Task myTask = new Task("Test Task 1","this is to test out our new class", date);
 
 
@@ -33,21 +30,31 @@ public class ToDoListDriver
 
         // System.out.println(myList);
 
+        // DueDate date = new DueDate(1, 22, 2022);
+        // DueDate date2 = new DueDate(6, 17, 2026);
+        // DueDate date3 = new DueDate(4, 9, 2025);
+        // DueDate date4 = new DueDate(8, 23, 2026);
+        ToDoList myList = new ToDoList();
+        System.out.println("How many tasks do you want to add? (Type a number)");
+        int count = keyboard.nextInt();
+        keyboard.close();
+        
+        for (int x = 0; x < count; x++){
+            Task newTask = userInput();
+            myList.add(newTask);
+        }
 
+        Schedule s = new Schedule();
 
+        s.addToDoList(myList);
+        System.out.println(s);
 
+    }
+
+    public static Task userInput(){
 
         Scanner keyboard = new Scanner(System.in);
-        boolean finished = false;
 
-        while (finished == false) {
-            System.out.println("Would you like to enter another Task?(Y/N): "); 
-            String item = keyboard.nextLine(); // reads statment to see if it should ask for another entry
-            if(item.equals("N")){
-                finished = true;
-            }
-            else if(item.equals("Y"))
-            {
                 System.out.println("Please enter the task name: "); 
                 String name = keyboard.nextLine();
                 System.out.println("Please add the day of the due date");
@@ -56,58 +63,11 @@ public class ToDoListDriver
                 int month = keyboard.nextInt();
                 System.out.println("Please enter the year of the due date: ");
                 int year = keyboard.nextInt();
-                System.out.println("Would you like this task to be repatable(Y/N): ");
-                String r = keyboard.nextLine();
-                TaskRepeat repeatable;
-                if(r.equals("Y")){
-                    repeatable = new TaskRepeat(true);
-                
-                }
-                else if (r.equals("N")){
-                    repeatable = new TaskRepeat(false);
-                }
-                
+                System.out.println("Please give a basic description of the task: ");
+                String description = keyboard.nextLine();
                 DueDate date = new DueDate(month,day,year);
-                Task myTask = new Task(name, "is due on", date, repeatable);
-
-                keyboard.nextLine(); // keyboard bug fix 
-                System.out.println(myTask);
-
-                ToDoList myList = new ToDoList();
-
-                myList.add(myTask);
-    
-            }
-
-        }
-        for(int i;i<myList.length();i++){
-            System.out.println("did you compleate this task?(Y/N): ");
-            String y = keyboard.nextLine();
-            if(y.equals("Y")){
-                myList[i].markComplete();
-                boolean h = myList[i].isRepeatable();
-                if(h=true){
-                    //call task repeat with the same name of task but with additional days added to due date
-                    // add this task to the task list
-                }
-            
-            }
-            else if (y.equals("N")){
-                
-            }
-        }
-        Schedule s = new Schedule();
-
-
-        s.addToDoList(myList);
-        System.out.println(s.isInSchedule(myOtherTask));
-        System.out.println(s);
-        newList.add(myTask1);
-        s.addToDoList(newList);
-        System.out.println(s);
-
-
-
-
+                Task myTask = new Task(name, description, date);
+                keyboard.close();
+                return myTask;
     }
 }

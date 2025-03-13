07d36047525aa198d8e8d587ad2d808955edddef
FranklinW10 <franklin.wohnoutka@drake.edu>
2025-03-13 10:03:42 -0500

    Update ToDoListDriver.java

--- ToDoListDriver.java
+++ ToDoListDriver.java
@@ -5,33 +5,7 @@ public class ToDoListDriver
     public static void main(String[] args) 
     {
 
-        // DueDate date = new DueDate(1, 22, 2022);
-        // DueDate date2 = new DueDate(6, 17, 2026);
-        // DueDate date3 = new DueDate(4, 9, 2025);
-        // DueDate date4 = new DueDate(8, 23, 2026);
-        // Task myTask = new Task("Test Task 1","this is to test out our new class", date);
 
-
-
-
-
-        //System.out.println(myTask);
-
-        //System.out.println("Here is the name of the task: "+myTask.getName());
-
-
-
-
-
-        // Task myOtherTask = new Task("Second Task!!!","Let's get a couple of tasks into the list", date2);
-        // Task myOtherTask1 = new Task("Third Task","Some dummy text for task 3", date3);
-        // Task myTask1 = new Task("Fourth Task","More dummy text for task 4", date4);
-
-        // myList.add(myOtherTask);
-        // myList.add(myOtherTask1);
-        
-
-        // System.out.println(myList);
 
         ToDoList myList = new ToDoList();
 
@@ -59,16 +33,20 @@ public class ToDoListDriver
                 System.out.println("Would you like this task to be repatable(Y/N): ");
                 String r = keyboard.nextLine();
                 TaskRepeat repeatable;
+                DueDate date = new DueDate(month,day,year);
+                Task myTask;
                 if(r.equals("Y")){
                     repeatable = new TaskRepeat(true);
-                
+                    System.out.println("how many days would you like to extend each time");
+                    int days = keyboard.nextInt();
+                    myTask = new Task(name, "is due on", date, repeatable.isRepeatable(), days);
                 }
-                else if (r.equals("N")){
+                else{
                     repeatable = new TaskRepeat(false);
+                    myTask = new Task(name, "is due on", date, repeatable.isRepeatable());
                 }
                 
-                DueDate date = new DueDate(month,day,year);
-                Task myTask = new Task(name, "is due on", date, repeatable.isRepeatable());
+                
 
                 keyboard.nextLine(); // keyboard bug fix 
                 System.out.println(myTask);
@@ -80,16 +58,11 @@ public class ToDoListDriver
             }
 
         }
-        for(int i;i<myList.getLength();i++){
+        for(int i=0;i<myList.getLength();i++){
             System.out.println("did you compleate this task?(Y/N): ");
             String y = keyboard.nextLine();
             if(y.equals("Y")){
-                myList.geTask(i).markComplete();
-                // boolean h = myList.geTask(i).isRepeatable();
-                // if(h=true){
-                //     //call task repeat with the same name of task but with additional days added to due date
-                //     // add this task to the task list
-                // }
+                myList.getTask(i).markComplete();
             
             }
             else if (y.equals("N")){

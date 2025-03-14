2a134d669d4984489c88d3e22fdd8fd98c04bc47
BradonTanner <164926595+BradonTanner@users.noreply.github.com>
2025-03-13 20:15:06 -0500

    Update NestedTask.java

--- NestedTask.java
+++ NestedTask.java
@@ -1,27 +1,69 @@
+/** 
+ * This class defines a subclass of task
+ * Is used to define an object that can be treated as a task but will be used to contain another class as well
+ * The nested class will be retreived in the driver to provide the user with the ability to make dependent tasks
+ * @params always takes the params for a task
+ * @param name as str
+ * @param description as str 
+ * @param repeat? as bool
+ * @param task to be nested as Task
+ * @Author Bradon T
+ * 
+ * 
+ * 
+ * 
+ * 
+*/
+
+
+
 public class NestedTask extends Task{
     private Task dependant;
 
-    public NestedTask(String n, String d , boolean r ,Task dependant) {//constructs a nested task without a due date
+    /** NestedTask1
+     * constructs a nested task without a due date
+     */
+    public NestedTask(String n, String d , boolean r ,Task dependant) {
         super(n,d,r);
         this.dependant = dependant;
     }
-    public NestedTask(String n, String d , DueDate date, boolean r, int da,  Task dependant) {//constructs a nested task with a due date
+    /** NestedTask2
+     * constructs a nested task with a due date
+     * @param date due as DueDate
+    */
+    public NestedTask(String n, String d , DueDate date, boolean r, int da,  Task dependant) {
         super(n,d,date,r,da);
         this.dependant = dependant;
     }
+    /** NestedTask3 
+     * constructs a nested task with a due date
+     * @param date due as DueDate
+    */
     public NestedTask(String n, String d ,DueDate date, boolean r, Task dependant) {
         super(n,d,date,r);
         this.dependant = dependant;
     }
-
-    public void setNested(Task dependant){//setter for the dependant task
+    /** setNested
+     * setter for the dependant task
+     * 
+     * @param dependant as Task
+     */
+    public void setNested(Task dependant){
         this.dependant = dependant;
     }
-    public Boolean isNested(){//returns true to show that the referenced task is nested
+    /** isNested
+     * returns true to show that the referenced task is nested
+     */
+    public Boolean isNested(){
         return true;
     }
+    /** 
+     * Getter for the dependent task
+     * 
+     * @return dependent task as Task
+     */
 
-    public Task getNested(){// will return the nested task once the independent task is completed
+    public Task getNested(){
         return this.dependant;
     }
 

597bd027ffd6cf052397a6efee05fdff3ab2ebd3
BradonTanner <164926595+BradonTanner@users.noreply.github.com>
2025-03-13 19:48:42 -0500

    Update NestedTask.java

--- NestedTask.java
+++ NestedTask.java
@@ -1,11 +1,11 @@
 public class NestedTask extends Task{
     private Task dependant;
 
-    public NestedTask(String n, String d , boolean r ,Task dependant) {
+    public NestedTask(String n, String d , boolean r ,Task dependant) {//constructs a nested task without a due date
         super(n,d,r);
         this.dependant = dependant;
     }
-    public NestedTask(String n, String d , DueDate date, boolean r, int da,  Task dependant) {
+    public NestedTask(String n, String d , DueDate date, boolean r, int da,  Task dependant) {//constructs a nested task with a due date
         super(n,d,date,r,da);
         this.dependant = dependant;
     }
@@ -14,14 +14,14 @@ public class NestedTask extends Task{
         this.dependant = dependant;
     }
 
-    public void setNested(Task dependant){
+    public void setNested(Task dependant){//setter for the dependant task
         this.dependant = dependant;
     }
-    public Boolean isNested(){
+    public Boolean isNested(){//returns true to show that the referenced task is nested
         return true;
     }
 
-    public Task getNested(){
+    public Task getNested(){// will return the nested task once the independent task is completed
         return this.dependant;
     }
 
